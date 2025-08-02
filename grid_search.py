9fa8b01c28859199fc8638d6c1cf715e67bc6bff
abadiania <34455949+abadiania@users.noreply.github.com>
2025-08-02 09:20:28 -0300

    verificação se results ja aconteceu

--- grid_search.py
+++ grid_search.py
@@ -1,13 +1,21 @@
 import subprocess
+import os
 from itertools import product
 
 learning_rates = [1e-5, 3e-5, 5e-5]
-batch_sizes = [64,128, 256, 512]
+batch_sizes = [64, 128, 256, 512]
 
 combinacoes = list(product(learning_rates, batch_sizes))
 
 for i, (lr, bs) in enumerate(combinacoes):
     output_dir = f"result/sup_bertimbau_dataset_trad_lr{lr}_bs{bs}"
+    results_file = os.path.join(output_dir, "train_results.txt")
+
+    # Verifica se o resultado já existe
+    if os.path.exists(results_file):
+        print(f"[SKIP] Já existe: {results_file}")
+        continue  # Pula para a próxima combinação
+
     cmd = [
         "python", "train.py",
         "--model_name_or_path", "bertimbau_local",
@@ -26,6 +34,8 @@ for i, (lr, bs) in enumerate(combinacoes):
         "--temp", "0.05",
         "--do_train",
         "--do_eval",
-        '--fp16'
+        "--fp16"
     ]
+
+    print(f"[RUN] Treinando com lr={lr}, bs={bs}")
     subprocess.run(cmd)
