eebe03713de3303ed39e123a4b02518d759a32af
abadiania <34455949+abadiania@users.noreply.github.com>
2025-08-04 12:32:31 -0300

    unsup grid

--- grid_search.py
+++ grid_search.py
@@ -32,7 +32,8 @@ batch_sizes = [64, 128, 256, 512]
 combinacoes = list(product(learning_rates, batch_sizes))
 
 for i, (lr, bs) in enumerate(combinacoes):
-    output_dir = f"result/sup_bertimbau_dataset_trad_lr{lr}_bs{bs}"
+    # output_dir = f"result/sup_bertimbau_dataset_trad_lr{lr}_bs{bs}"
+    output_dir = f"result/unsup_bertimbau_dataset_trad_lr{lr}_bs{bs}"
     results_file = os.path.join(output_dir, "train_results.txt")
 
     # Verifica se o resultado já existe
@@ -42,12 +43,31 @@ for i, (lr, bs) in enumerate(combinacoes):
         continue  # Pula para a próxima combinação
     print(f"[RUN] Treinando com lr={lr}, bs={bs}")
     enviar_mensagem_webhook(f"[RUN] Treinando com lr={lr}, bs={bs}")
+    # cmd = [
+    #     "python", "train.py",
+    #     "--model_name_or_path", "bertimbau_local",
+    #     "--train_file", "data/portuguese_nli_trad/pt_nli_for_simcse.csv",
+    #     "--output_dir", output_dir,
+    #     "--num_train_epochs", "3",
+    #     "--per_device_train_batch_size", str(bs),
+    #     "--learning_rate", str(lr),
+    #     "--max_seq_length", "32",
+    #     "--evaluation_strategy", "steps",
+    #     "--metric_for_best_model", "stsb_spearman",
+    #     "--load_best_model_at_end",
+    #     "--eval_steps", "250",
+    #     "--pooler_type", "cls",
+    #     "--overwrite_output_dir",
+    #     "--temp", "0.05",
+    #     "--do_train",
+    #     "--do_eval",
+        # "--fp16"]
     cmd = [
         "python", "train.py",
         "--model_name_or_path", "bertimbau_local",
-        "--train_file", "data/portuguese_nli_trad/pt_nli_for_simcse.csv",
+        "--train_file", "data/portuguese_wiki1m_real/pt_wiki1m_for_simcse_real.txt",
         "--output_dir", output_dir,
-        "--num_train_epochs", "3",
+        "--num_train_epochs", "1",
         "--per_device_train_batch_size", str(bs),
         "--learning_rate", str(lr),
         "--max_seq_length", "32",
@@ -56,6 +76,7 @@ for i, (lr, bs) in enumerate(combinacoes):
         "--load_best_model_at_end",
         "--eval_steps", "250",
         "--pooler_type", "cls",
+        "--mlp_only_train",
         "--overwrite_output_dir",
         "--temp", "0.05",
         "--do_train",
